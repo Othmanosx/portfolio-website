40cbe63e30a14807579b9521e5fc76bf45114fd3
Othman Shareef <othmanosx@gmail.com>
2021-03-08 18:42:49 +0300

    add scss and components

--- src/elements/contact/ContactThree.jsx
+++ src/elements/contact/ContactThree.jsx
@@ -1,83 +1,156 @@
 import React, { Component } from "react";
+import emailjs from "emailjs-com";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
 
-class ContactThree extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            rnName: '',
-            rnEmail: '',
-            rnSubject: '',
-            rnMessage: '',
-        };
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+class ContactThree extends Component {
+  handleClick = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
     }
-    render(){
-        return(
-            <div className="contact-form--1">
-                <div className="container">
-                    <div className="row row--35 align-items-start">
-                        <div className="col-lg-6 order-2 order-lg-1">
-                            <div className="section-title text-left mb--50">
-                                <h2 className="title">{this.props.contactTitle}</h2>
-                                <p className="description">I am available for freelance work. Connect with me via phone: <a href="tel:+8801923088574">01923 088574</a> or email:
-                                    <a href="mailto:admin@example.com"> admin@example.com</a> </p>
-                            </div>
-                            <div className="form-wrapper">
-                                <form>
-                                    <label htmlFor="item01">
-                                        <input
-                                            type="text"
-                                            name="name"
-                                            id="item01"
-                                            value={this.state.rnName}
-                                            onChange={(e)=>{this.setState({rnName: e.target.value});}}
-                                            placeholder="Your Name *"
-                                        />
-                                    </label>
 
-                                    <label htmlFor="item02">
-                                        <input
-                                            type="text"
-                                            name="email"
-                                            id="item02"
-                                            value={this.state.rnEmail}
-                                            onChange={(e)=>{this.setState({rnEmail: e.target.value});}}
-                                            placeholder="Your email *"
-                                        />
-                                    </label>
+    this.setState({ open: false });
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      rnName: "",
+      rnEmail: "",
+      rnSubject: "",
+      rnMessage: "",
+      open: false
+    };
+  }
+  sendEmail = (e) => {
+    e.preventDefault();
 
-                                    <label htmlFor="item03">
-                                        <input
-                                            type="text"
-                                            name="subject"
-                                            id="item03"
-                                            value={this.state.rnSubject}
-                                            onChange={(e)=>{this.setState({rnSubject: e.target.value});}}
-                                            placeholder="Write a Subject"
-                                        />
-                                    </label>
-                                    <label htmlFor="item04">
-                                        <textarea
-                                            type="text"
-                                            id="item04"
-                                            name="message"
-                                            value={this.state.rnMessage}
-                                            onChange={(e)=>{this.setState({rnMessage: e.target.value});}}
-                                            placeholder="Your Message"
-                                        />
-                                    </label>
-                                    <button className="rn-button-style--2 btn-solid" type="submit" value="submit" name="submit" id="mc-embedded-subscribe">Submit</button>
-                                </form>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 order-1 order-lg-2">
-                            <div className="thumbnail mb_md--30 mb_sm--30">
-                                <img src={`${this.props.contactImages}`} alt="trydo"/>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    emailjs
+      .send(
+        "service_oc8qznf",
+        "template_oc0q9be",
+        {
+          rnEmail: this.state.rnEmail,
+          rnSubject: this.state.rnSubject,
+          rnMessage: this.state.rnMessage,
+          rnName: this.state.rnName
+        },
+        "user_HDJ1SCLZ6I6ByO7Ey8D36"
+      )
+      .then(
+        (result) => {
+          this.handleClick();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    this.setState({ rnName: "", rnEmail: "", rnSubject: "", rnMessage: "" });
+  };
+  render() {
+    return (
+      <div className="contact-form--1">
+        <div className="container">
+          <center>
+            <div className="col-lg-8 order-2 order-lg-1">
+              <div className="section-title text-left mb--50">
+                <h2 className="title">{this.props.contactTitle}</h2>
+                <p className="description">
+                  I am available for full time, part time and freelance work.
+                  Connect with me via email:
+                  <a href="mailto:admin@example.com"> admin@example.com</a>{" "}
+                </p>
+              </div>
+              <div className="form-wrapper">
+                <form onSubmit={this.sendEmail}>
+                  <label
+                    style={{ float: "left", width: "48%" }}
+                    htmlFor="item01"
+                  >
+                    <input
+                      type="text"
+                      name="name"
+                      id="item01"
+                      value={this.state.rnName}
+                      onChange={(e) => {
+                        this.setState({ rnName: e.target.value });
+                      }}
+                      placeholder="Your Name *"
+                    />
+                  </label>
+
+                  <label
+                    style={{ float: "right", width: "48%" }}
+                    htmlFor="item02"
+                  >
+                    <input
+                      type="text"
+                      name="email"
+                      id="item02"
+                      value={this.state.rnEmail}
+                      onChange={(e) => {
+                        this.setState({ rnEmail: e.target.value });
+                      }}
+                      placeholder="Your email *"
+                    />
+                  </label>
+
+                  <label htmlFor="item03">
+                    <input
+                      type="text"
+                      name="subject"
+                      id="item03"
+                      value={this.state.rnSubject}
+                      onChange={(e) => {
+                        this.setState({ rnSubject: e.target.value });
+                      }}
+                      placeholder="Write a Subject"
+                    />
+                  </label>
+                  <label htmlFor="item04">
+                    <textarea
+                      type="text"
+                      id="item04"
+                      name="message"
+                      value={this.state.rnMessage}
+                      onChange={(e) => {
+                        this.setState({ rnMessage: e.target.value });
+                      }}
+                      placeholder="Your Message"
+                    />
+                  </label>
+                  <button
+                    className="rn-button-style--2 btn-solid"
+                    type="submit"
+                    value="submit"
+                    name="submit"
+                    id="mc-embedded-subscribe"
+                  >
+                    Submit
+                  </button>
+                </form>
+              </div>
             </div>
-        )
-    }
+          </center>
+        </div>
+        <Snackbar
+          open={this.state.open}
+          autoHideDuration={3000}
+          onClose={this.handleClose}
+        >
+          <Alert onClose={this.handleClose} severity="success">
+            Email sent!
+          </Alert>
+        </Snackbar>
+      </div>
+    );
+  }
 }
 export default ContactThree;
--- src/elements/portfolio/PortfolioList.jsx
+++ src/elements/portfolio/PortfolioList.jsx
@@ -1,66 +1,64 @@
 import React, { Component } from "react";
 
 const PortfolioListContent = [
-    {
-        image: 'image-1',
-        category: 'Development',
-        title: 'Getting tickets to the big show'
-    },
-    {
-        image: 'image-2',
-        category: 'Development',
-        title: 'Getting tickets to the big show'
-    },
-    {
-        image: 'image-3',
-        category: 'Development',
-        title: 'Getting tickets to the big show'
-    },
-    {
-        image: 'image-4',
-        category: 'Development',
-        title: 'Getting tickets to the big show'
-    },
-    {
-        image: 'image-3',
-        category: 'Development',
-        title: 'Getting tickets to the big show'
-    },
-    {
-        image: 'image-4',
-        category: 'Development',
-        title: 'Getting tickets to the big show'
-    }
-]
+  {
+    image: "image-1",
+    category: "React JS",
+    title: "Weather App",
+    link: "https://weather-app-othman.netlify.app/"
+  },
+  {
+    image: "image-2",
+    category: "Javascript",
+    title: "iTECH Website",
+    link: "https://www.itechnopro.com/"
+  },
+  {
+    image: "image-3",
+    category: "React JS",
+    title: "Hangman Game",
+    link: "https://hangman-sand-team.netlify.app/"
+  }
+];
 
-class PortfolioList extends Component{
-    render(){
-        const {column , styevariation } = this.props;
-        const list = PortfolioListContent.slice(0 , this.props.item);
-        return(
-            <React.Fragment> 
-                {list.map((value , index) => (
-                    <div className={`${column}`} key={index}>
-                        <div className={`portfolio ${styevariation}`}>
-                            <div className="thumbnail-inner">
-                                <div className={`thumbnail ${value.image}`}></div>
-                                <div className={`bg-blr-image ${value.image}`}></div>
-                            </div>
-                            <div className="content">
-                                <div className="inner">
-                                    <p>{value.category}</p>
-                                    <h4><a href="/portfolio-details">{value.title}</a></h4>
-                                    <div className="portfolio-button">
-                                        <a className="rn-btn" href="/portfolio-details">View Details</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                ))}
-               
-            </React.Fragment>
-        )
-    }
+class PortfolioList extends Component {
+  render() {
+    const { column, styevariation } = this.props;
+    const list = PortfolioListContent.slice(0, this.props.item);
+    return (
+      <React.Fragment>
+        {list.map((value, index) => (
+          <div className={`${column}`} key={index}>
+            <div className={`portfolio ${styevariation}`}>
+              <div className="thumbnail-inner">
+                <div className={`thumbnail ${value.image}`}></div>
+                <div className={`bg-blr-image ${value.image}`}></div>
+              </div>
+              <div className="content">
+                <div className="inner">
+                  <p>{value.category}</p>
+                  <h4>
+                    <a href={value.link} rel="noreferrer" target="_blank">
+                      {value.title}
+                    </a>
+                  </h4>
+                  <div className="portfolio-button">
+                    <a
+                      className="rn-btn"
+                      href={value.link}
+                      rel="noreferrer"
+                      target="_blank"
+                    >
+                      View Details
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </React.Fragment>
+    );
+  }
 }
 export default PortfolioList;
