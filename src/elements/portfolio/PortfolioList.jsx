e370a22d75c67444d7aedf208e5ee5b1e814a3cd
Othman Shareef <othmanosx@gmail.com>
2021-03-27 18:11:43 +0300

    Feat: add new projects and repo button

--- src/elements/portfolio/PortfolioList.jsx
+++ src/elements/portfolio/PortfolioList.jsx
@@ -5,20 +5,35 @@ const PortfolioListContent = [
     image: "image-1",
     category: "React JS",
     title: "Weather App",
-    link: "https://weather-app-othman.netlify.app/"
+    link: "https://weather-app-othman.netlify.app/",
+    repo: "https://github.com/Othmanosx/weather_app_js",
   },
   {
     image: "image-2",
     category: "Javascript",
     title: "iTECH Website",
-    link: "https://www.itechnopro.com/"
+    link: "https://www.itechnopro.com/",
   },
   {
     image: "image-3",
     category: "React JS",
     title: "Hangman Game",
-    link: "https://hangman-sand-team.netlify.app/"
-  }
+    link: "https://hangman-sand-team.netlify.app/",
+    repo: "https://github.com/Othmanosx/hangman_react_js",
+  },
+  {
+    image: "image-4",
+    category: "Javascript",
+    title: "iTECH APPS",
+    link: "https://www.itechnoapps.com/",
+  },
+  {
+    image: "image-5",
+    category: "React JS",
+    title: "FindYours (in development)",
+    link: "",
+    repo: "https://github.com/murtaja1/find_yours",
+  },
 ];
 
 class PortfolioList extends Component {
@@ -42,15 +57,31 @@ class PortfolioList extends Component {
                       {value.title}
                     </a>
                   </h4>
-                  <div className="portfolio-button">
-                    <a
-                      className="rn-btn"
-                      href={value.link}
-                      rel="noreferrer"
-                      target="_blank"
-                    >
-                      View Details
-                    </a>
+                  <div className="postButtons">
+                    {value.link ? (
+                      <div className="portfolio-button">
+                        <a
+                          className="rn-btn"
+                          href={value.link}
+                          rel="noreferrer"
+                          target="_blank"
+                        >
+                          Live
+                        </a>
+                      </div>
+                    ) : null}
+                    {value.repo ? (
+                      <div className="portfolio-button">
+                        <a
+                          className="rn-btn"
+                          href={value.repo}
+                          rel="noreferrer"
+                          target="_blank"
+                        >
+                          Repo
+                        </a>
+                      </div>
+                    ) : null}
                   </div>
                 </div>
               </div>
