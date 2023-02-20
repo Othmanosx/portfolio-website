e82cef78e2f1fe2d44cc9da0977bfa2927253f8b
Othman Shareef <othmanosx@gmail.com>
2023-02-21 00:51:35 +0300

    Fix: prioritize hero image loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,6 +37,7 @@ export default function Home() {
               backgroundRepeat: "no-repeat",
               objectFit: "cover",
             }}
+            priority
             alt="personal photo"
           />
           <Particles />
